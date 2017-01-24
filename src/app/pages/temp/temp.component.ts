7aeab49a9dffe42f5a6143c4f5915b7d5df76354
iiAndrew <iiAndrew@users.noreply.github.com>
2017-01-24 18:06:23 +0300

    Fix AOT compilation

--- src/app/pages/temp/temp.component.ts
+++ src/app/pages/temp/temp.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { CardModule } from '../../components/cards/card.component';
 
 @Component({
   templateUrl: 'temp.template.html'
 })
 
-export class DataGridComponent {
-  constructor() {}
+export class TempComponent {
+  constructor() { }
 }
+
+
+@NgModule({
+  imports: [CardModule],
+  declarations: [TempComponent]
+})
+export class TempModule { }
